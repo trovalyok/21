c474003d6850896518c6d3b53b33b73806be0c9b
Valentyna Trokhina <trokhina.v@gmail.com>
2024-04-10 01:41:42 +0300

    Another account has been added to the test

--- test_2.py
+++ test_2.py
@@ -8,6 +8,8 @@ class TestBankUpdate(unittest.TestCase):
         account_s_1 = SavingsAccount(1200, 'GB29 NWBK 6016 1331 9268 19', 2)
         account_s_2 = SavingsAccount(1500, 'FI21 1234 5600 0007 85', 2.5)
         account_c_1 = CurrentAccount(1050, 'DE89 3704 0044 0532 0130 00', -200)
+        account_c_2 = CurrentAccount(1400, 'SE45 5000 0000 0583 9825 7466',
+                                     -100)
 
         with patch('builtins.print') as mock_print:
             bank = Bank([account_s_1, account_s_2, account_c_1])
@@ -16,6 +18,7 @@ class TestBankUpdate(unittest.TestCase):
 
             self.assertEqual(account_s_1.get_balance(), 1224.0)
             self.assertEqual(account_s_2.get_balance(), 1537.5)
+            self.assertEqual(account_c_2.get_balance(), 1400)
 
             mock_print.assert_called_once_with('DE89 3704 0044 0532 0130 00\
  in overdraft')
